a007e653a74f1e9a23105b95f8b8568df115157f
Marcus Miris <marcusmiris@reinodedeus.org>
2017-12-01 10:55:54 -0200

    criação do WithStatement

---
+++ Miris.CSharpExtensions/WithStatement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Miris.CSharpExtensions
+{
+    public static partial class CSharpExtensions
+    {
+        /// <summary>
+        ///     VB-like With Statement.
+        /// </summary>
+        /// <example>
+        /// with(new List(), _ => {
+        ///     _.Add(1);
+        ///     _.Add(2);
+        /// });
+        /// </example>
+        public static T with<T>(T Object, Action<T> then)
+        {
+            then(Object);
+            return Object;
+        }
+
+        /// <summary>
+        ///     VB-like With Statement.
+        /// </summary>
+        /// <example>
+        /// var qtd = with(new List(), _ => {
+        ///     _.Add(1);
+        ///     return _.Count;
+        /// });
+        /// </example>
+        public static TReturn with<T, TReturn>(T Object, Func<T, TReturn> then)
+            => then(Object);
+
+    }
+}
